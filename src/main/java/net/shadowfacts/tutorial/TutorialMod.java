5b0d7a4b068af9629fa649a37c40feceaf37191f
xenonni <osabarrowes@yahoo.com>
2016-12-01 15:32:54 -0700

    Added framework for proxy classes and items as per shadowfacts tutorial http://shadowfacts.net/tutorials/forge-modding-1102/basic-items/ , added test item ingotCopper with no texture. Very exciting!

---
+++ src/main/java/net/shadowfacts/tutorial/TutorialMod.java
@@ -0,0 +1,39 @@
+package net.shadowfacts.tutorial;
+
+import net.shadowfacts.tutorial.proxy.*;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = TutorialMod.modId, name = TutorialMod.name, version = TutorialMod.version, acceptedMinecraftVersions = "[1.10.2]")
+public class TutorialMod {
+
+	public static final String modId = "tutorial";
+	public static final String name = "Tutorial Mod";
+	public static final String version = "1.0.0";
+
+	@Mod.Instance(modId)
+	public static TutorialMod instance;
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		System.out.println(name + " is loading!");
+		ModItems.init();
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
+	
+	@SidedProxy(serverSide = "net.shadowfacts.tutorial.proxy.CommonProxy", clientSide = "net.shadowfacts.tutorial.proxy.ClientProxy")
+	public static CommonProxy proxy;
+
+}
